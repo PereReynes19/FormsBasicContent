eeab18139f478469366c2c2add660087f8fbdd08
Prova <prova@gmail.com>
2020-10-13 11:44:21 +0200

    Finished the exercice one.
    Added the function when you click the label.
    Added the personal message in the messagebox.

--- FormsBasicContent_FormsLabel/Form1.cs
+++ FormsBasicContent_FormsLabel/Form1.cs
@@ -16,5 +16,10 @@ namespace FormsBasicContent_FormsLabel
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
